c9460135f6fff4e2a27e4e3981b42dd44d8c179c
Elias Meireles <eliasmflilico@gmail.com>
2022-05-07 17:55:27 -0300

    Project initialization

---
+++ src/main/kotlin/com/example/springwebfluxprojectreactor/SpringWebFluxProjectReactorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springwebfluxprojectreactor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebFluxProjectReactorApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebFluxProjectReactorApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springwebfluxprojectreactor/SpringWebFluxProjectReactorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springwebfluxprojectreactor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringWebFluxProjectReactorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
